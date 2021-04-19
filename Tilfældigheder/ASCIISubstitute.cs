a34fc7b97a9a6a8e67fe5bbb748c8ca492e695a1
Leo Jensen <lion0005@zbc.dk>
2021-04-19 12:11:48 +0200

    Add project files.

---
+++ Tilfældigheder/ASCIISubstitute.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tilfældigheder
+{
+    public class ASCIISubstitute
+    {
+        
+        private byte letter;
+        private int key;
+        private byte[] buffer;
+        private char[] processedMSG;
+
+        public string ASCIIEncrypt(string text)
+        {
+            key = 2;
+
+            buffer = Encoding.ASCII.GetBytes(text);
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                // seperate the letters
+                letter = buffer[i];
+
+                //shift the letters according to key
+                letter = (byte)(letter + key);
+
+
+                //subtract 90 if there's an overflow
+                //add 90 if there's an underflow
+                if (letter > 122)
+                {
+                    letter = (byte)(letter - 90);
+                }
+                else if (letter < 32)
+                {
+                    letter = (byte)(letter + 90);
+                }
+
+                //re-store the variable data
+                buffer[i] = letter;
+
+
+                processedMSG = Encoding.ASCII.GetChars(buffer);
+                
+            }
+
+            return new string(processedMSG);
+        }
+
+        public string ASCIIDecrypt(string text)
+        {
+            key = 2;
+
+            buffer = Encoding.ASCII.GetBytes(text);
+
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                // seperate the letters
+                letter = buffer[i];
+
+                //shift the letters according to key
+                letter = (byte)(letter - key);
+
+
+                //subtract 90 if there's an overflow
+                //add 90 if there's an underflow
+                if (letter > 122)
+                {
+                    letter = (byte)(letter - 90);
+                }
+                else if (letter < 32)
+                {
+                    letter = (byte)(letter + 90);
+                }
+
+                //re-store the variable data
+                buffer[i] = letter;
+
+                //re-inccode buffer bytes to char
+                processedMSG = Encoding.ASCII.GetChars(buffer);
+
+            }
+            // returns processedMSG as a new string
+            return new string(processedMSG);
+        }
+    }
+}
---
+++ Tilfældigheder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tilfældigheder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ASCIISubstitute ascii = new ASCIISubstitute();
+            Console.WriteLine("Write a msg");
+            string text = Console.ReadLine();
+            Console.WriteLine("your text: " + text);
+            for (int i = 0; i < 3; i++)
+            {
+                text = ascii.ASCIIEncrypt(text);
+                Console.WriteLine($"encryption number {i+1}: {text} ");
+            }
+            Console.WriteLine("Encrypted text: " + text);
+            Console.WriteLine("Decrypt your text: ");
+            Console.ReadKey();
+            Console.WriteLine("your encrypted text: " + text);
+            for (int i = 0; i < 3; i++)
+            {
+                text = ascii.ASCIIDecrypt(text);
+                Console.WriteLine($"Decryption number {i+1}: {text}");
+            }
+            Console.WriteLine("your decrypted text: " + text);
+            Console.ReadKey();
+        }
+    }
+}
